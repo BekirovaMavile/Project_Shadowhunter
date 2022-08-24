13ee748eed3e2e87e9bb486de020e83250f01297
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-08-24 14:17:41 +0300

    Маршрутизатор

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Страница Клэри Фэйрчайлд */
+router.get('/clary', function(req, res, next) {
+  res.send("<h1>Страница Клэри Фэйрчайлд</h1>")
 });
-
+/* Страница Джейса Эрондейла */
+router.get('/jace', function(req, res, next) {
+  res.send("<h1>Страница Джейса Эрондейла</h1>")
+});
+/* Страница Алека Лайтвуда */
+router.get('/alec', function(req, res, next) {
+  res.send("<h1>Страница Алека Лайтвуда</h1>")
+});
+/* Страница Магнуса Бейна */
+router.get('/magnus', function(req, res, next) {
+  res.send("<h1>Страница Магнуса Бейна</h1>")
+});
+/* Страница Изабель Лайтвуд */
+router.get('/isabel', function(req, res, next) {
+  res.send("<h1>Страница Изабель Лайтвуд</h1>")
+});
 module.exports = router;
