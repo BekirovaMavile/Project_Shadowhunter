c10b9ecd7e368d85e4d7df6976dee05db4faeb2b
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-08-30 13:44:00 +0300

    Данные приложения

--- createDB.js
+++ createDB.js
@@ -1,15 +1,16 @@
 var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data
 
 const uri = "mongodb://localhost:27017/"
 const client = new MongoClient(uri)
 async function run() {
     try  {
         await client.connect();
-        var database = client.db("threecats");
+        var database = client.db("shadowhunter");
         database.dropDatabase()
-        database = client.db("threecats");
-        const cats = database.collection("cats");
-        const result = await cats.insertOne({name: "Пушок"});
+        database = client.db("shadowhunter");
+        const hunters = database.collection("hunters");
+        const result = await hunters.insertMany(data);
         for (const key in result) {
             console.log(`${key}: ${result[key]}`);
             }
---
+++ data.js
@@ -0,0 +1,33 @@
+var data = [{
+        title: 'Клэри Фэйрчайлд',
+        nick: 'clary',
+        avatar: '/images/clary.webp',
+        desc: 'Сумеречная охотница, которая может воссоздавать мощные древние руны, не написанные в Серой Книге'
+    },
+    {
+        title: 'Джейс Эрондейл',
+        nick: 'jace',
+        avatar: '/images/jace.webp',
+        desc: 'Cо-глава Института Нью-Йорка. Может активировать руны без стеле'
+    },
+    {
+        title: 'Алек Лайтвуд',
+        nick: 'alec',
+        avatar: '/images/alec.webp',
+        desc: 'Консул Конклава-в-изгнании'
+    },
+    {
+        title: 'Магнус Бейн',
+        nick: 'magnus',
+        avatar: '/images/magnus.webp',
+        desc: 'Верховный Маг Бруклина'
+    },
+    {
+        title: 'Изабель Лайтвуд',
+        nick: 'isabel',
+        avatar: '/images/isabel.webp',
+        desc: 'Сумеречная охотница. Младшая сестра Алека'
+    }
+];
+module.exports.data = data;
+console.log("here")
