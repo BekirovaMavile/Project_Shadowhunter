71ff7da079d6790904bd562ef3370a4d04545391
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-09-01 17:04:51 +0300

    Создание модели данных приложения

--- mongooseStudy.js
+++ mongooseStudy.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/mongoose')
+mongoose.connect('mongodb://localhost/test')
+var Hero = require("./models/hunter").Hunter
 
-var schema = mongoose.Schema({name:String})
-
-schema.methods.say = function(){
-    console.log(this.get("name") + " сказал, что у него есть парабатай")
-}
-var hunters = mongoose.model('hunters', schema)
-
-var hunt = new hunters({ name: 'Алек' })
-hunt.save(function (err) {
-    hunt.say()
+var hero = new Hero({
+    title: "Алек Лайтвуд",
+    nick: "alec"
 })
+
+console.log(hero)
+hero.save(function(){
+    console.log(arguments)
+})
+
