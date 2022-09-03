765b2a83c00c4c5a5a0f3bf7c6067504ad7a2a48
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-09-03 12:05:44 +0300

    Чистка кода

--- routes/heroes.js
+++ routes/heroes.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 var Hero = require("../models/hunter").Hunter
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маошрутов, начинающихся с heroes');
-});
 
 /* Страница героев */
 router.get('/:nick', function(req, res, next) {
