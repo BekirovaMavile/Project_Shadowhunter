2dde78200aae3c90b10a54aee323749e2486392a
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-09-04 19:07:31 +0300

    Favicon

--- routes/heroes.js
+++ routes/heroes.js
@@ -1,19 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var Hero = require("../models/hunter").Hunter
+var async = require("async")
 
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('Новый маршрутизатор, для маошрутов, начинающихся с heroes')
+});
 
 /* Страница героев */
 router.get('/:nick', function(req, res, next) {
-    Hero.findOne({nick:req.params.nick}, function(err,hero){
-        if(err) return next(err)
-        if(!hero) return next(new Error("Нет такого героя в этой книжке"))
-        res.render('hero', {
-            title: hero.title,
-            picture: hero.avatar,
-            desc: hero.desc
+    async.parallel([
+            function(callback){
+                Hero.findOne({nick:req.params.nick}, callback)
+            },
+            function(callback){
+                Hero.find({},{_id:0,title:1,nick:1},callback)
+            }
+        ],
+        function(err,result){
+            if(err) return next(err)
+            var hero = result[0]
+            var heroes = result[1] || []
+            if(!hero) return next(new Error("Нет такого героя в этой книжке"))
+            res.render('hero', {
+                title: hero.title,
+                picture: hero.avatar,
+                desc: hero.desc,
+                menu: heroes
+            });
         })
-    })
 })
 
-module.exports = router;
+module.exports = router
+
