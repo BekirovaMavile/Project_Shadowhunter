80853126e15093234ff2a43e38c40026b7e29b99
Mavile Bekirova <mavilebekirova@MacBook-Air-Mavile.local>
2022-09-08 14:10:08 +0300

    Команда записи в cookie

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,7 @@ var Hero = require("../models/hunter").Hunter
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-        res.render('index', {
-                                title: 'Сумеречные охотники',
-                                menu: menu
-                            });
+            res.cookie('greeting', 'Hi!!!').render('index', { title: 'Сумеречные охотники', menu:menu });
     })
 });
 
