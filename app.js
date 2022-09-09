5d67aacc77745621bb6cffd1dd2e0da3e090ec6a
Mavile Bekirova <mavilebekirova@MacBook-Air-Mavile.local>
2022-09-09 10:30:23 +0300

    Создание счетчика посещения страницы сайта

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ app.use(session({
     store: MongoStore.create({mongoUrl:'mongodb://localhost/shadowhunter'})
 }))
 
+app.use(function(req,res,next){
+req.session.counter = req.session.counter + 1 || 1
+next()
+})
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var Hero = require("../models/hunter").Hunter
 router.get('/', function(req, res, next) {
     Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
             req.session.greeting = "Hi!!!"
-    res.render('index', { title: 'Сумеречные охотники', menu:menu });
+    res.render('index', { title: 'Сумеречные охотники', menu:menu, counter:req.session.counter  });
     })
 });
 
