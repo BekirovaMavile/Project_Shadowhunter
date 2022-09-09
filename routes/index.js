eb5a0905bde7c76602c8225e3e9f2af60f77d882
Mavile Bekirova <mavilebekirova@MacBook-Air-Mavile.local>
2022-09-09 10:11:11 +0300

    Сохранение session в mongodb

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var Hero = require("../models/hunter").Hunter
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
-            res.cookie('greeting', 'Hi!!!').render('index', { title: 'Сумеречные охотники', menu:menu });
+            req.session.greeting = "Hi!!!"
+    res.render('index', { title: 'Сумеречные охотники', menu:menu });
     })
 });
 
