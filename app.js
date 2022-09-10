62aa1db1e966e0281e1585ed519a315164920571
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-09-10 11:11:25 +0300

    Глобальная переменная для навигации

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title:"404", picture:"./public/images/error.jpeg", menu: []});
+  res.render('error', {title:"404", picture:"/images/error.jpeg"});
 });
 
 module.exports = app;
--- routes/heroes.js
+++ routes/heroes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Hero = require("../models/hunter").Hunter
-var async = require("async")
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,27 +9,41 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница героев */
-router.get('/:nick', function(req, res, next) {
-    async.parallel([
-            function(callback){
-                Hero.findOne({nick:req.params.nick}, callback)
-            },
-            function(callback){
-                Hero.find({},{_id:0,title:1,nick:1},callback)
-            }
-        ],
-        function(err,result){
-            if(err) return next(err)
-            var hero = result[0]
-            var heroes = result[1] || []
-            if(!hero) return next(new Error("Нет такого героя в этой книжке"))
-            res.render('hero', {
-                title: hero.title,
-                picture: hero.avatar,
-                desc: hero.desc,
-            });
-        })
-})
+router.get('/:nick', function(req, res, next) {[
+function(callback){
+    Hero.findOne({nick:req.params.nick})
+},
+    function(err,result){
+        if(err) return next(err)
+        var hero = result[0]
+        if(!hero) return next(new Error("Нет такого героя в этой книжке"))
+        res.render('hero', {
+                 title: hero.title,
+                 picture: hero.avatar,
+                 desc: hero.desc,
+             });
+         }]})
+//     async.parallel([
+//             function(callback){
+//                 Hero.findOne({nick:req.params.nick}, callback)
+//             },
+//             function(callback){
+//                 Hero.find({},{_id:0,title:1,nick:1},callback)
+//             }
+//         ],
+//         function(err,result){
+//             if(err) return next(err)
+//             var hero = result[0]
+//             var heroes = result[1] || []
+//             if(!hero) return next(new Error("Нет такого героя в этой книжке"))
+//             res.render('hero', {
+//                 title: hero.title,
+//                 picture: hero.avatar,
+//                 desc: hero.desc,
+//                 menu: heroes
+//             });
+//         })
+// })
 
 module.exports = router
 
