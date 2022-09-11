8045564063b0130d3898255bdcd5c5b7dd1676bb
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-09-11 12:24:18 +0300

    Создание страницы регистрации

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,10 @@ router.get('/', function(req, res, next) {
             req.session.greeting = "Hi!!!"
     res.render('index', { title: 'Сумеречные охотники', counter:req.session.counter  });
 });
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+res.render('logreg',{title: 'Вход'});
+});
+
 
 module.exports = router;
