d6f3006a07f1807143e7ec26a002b5bcce43b1b6
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-12-19 19:47:23 +0300

    Отображение данных в браузере

---
+++ routes/hunter.js
@@ -0,0 +1,40 @@
+// import db from "../database.js";
+var db = require('../database.js');
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Новый маршрутизатор, для маршрутов, начинающихся с hunter');
+});
+
+/* Страница охотников */
+router.get('/:nick', function(req, res, next) {
+    db.query(`SELECT * FROM hunter WHERE hunter.nick = '${req.params.nick}'`, (err, hunters) => {             
+        if(err) {
+            console.log(err);
+            if(err) return next(err)
+        } else {
+            // console.log(hunter[0])
+            if(hunters.length == 0) return next(new Error("Нет такого охотника в книге"))
+            var hunter = hunters[0];
+            res.render('hero', {
+            title: hunter.title,
+            picture: hunter.avatar,
+            desc: hunter.desc
+        })
+            // result(null, results);
+        }
+    })
+    // Cat.findOne({nick:req.params.nick}, function(err,cat){
+    //     if(err) return next(err)
+    //     if(!hunter) return next(new Error("Нет такого охотника в книге"))
+    //     res.render('hunter', {
+    //         title: hunter.title,
+    //         picture: hunter.avatar,
+    //         desc: hunter.desc
+    //     })
+    // })
+})
+
+module.exports = router;
