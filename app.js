6415f0a67da02a98be8eadf9f6de9ff013d18e29
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2022-12-23 13:15:26 +0300

    Express-mysql-session

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var mysql = require('mysql2')
-// var db = require('./database');
+var mysql      = require('mysql2');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,10 +14,33 @@ var hunter = require('./routes/hunter');
 
 var app = express();
 
+// var options = {
+//    host     : '127.0.0.1',
+//   port: '3306',
+//   user     : 'root',
+//   password : '',
+//   database: 'shadowhunter'
+// };
+// var connection = mysql.createConnection(options)
+// var sessionStore = new MySQLStore( options, connection);
+
 // view engine setup
 app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// app.use(cookieParser());
+// app.use(session({
+//                   secret: 'keyboardCat',
+//                   key: 'sid',
+//                   store: sessionStore,
+//                   resave: true,
+//                   saveUninitialized: true,
+//                   cookie: { path: '/',
+//                             httpOnly: true, 
+//                             maxAge: 60*1000
+//                           }
+//                   }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,18 +48,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(function(req,res,next){
-// res.locals.nav = []
-
-// // router.get({title, nick}, function(req, res, next) {
-//     db.query(`SELECT * FROM hunter WHERE hunter.nick = '${req.params.nick}' and hunter.title = '${req.params.title}`, (err, hunters) => {  
-//         if(err) throw err
-//         res.locals.nav = hunters
-//         next()
-//     })
-//   // })
-// })
 app.use(require("./middleware/createMenu.js"))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
